4bc42ad58ff51096e115655f8d2d807a55e4de46
zsoltpinter <90318352+zsoltpinter@users.noreply.github.com>
2023-11-23 13:22:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ import streamlit as st
 st.title("Kalkulátor")
 st.write("---")
 
-szam1 = st.number_input(label = "Adjá számot")
-szam2 = st.number_input(label = "Adjá még egy számot")
+szam1 = st.number_input(label = "szám")
+szam2 = st.number_input(label = "szám")
+
+
