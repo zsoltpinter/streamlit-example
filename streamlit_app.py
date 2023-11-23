ae3cc2744456eb7476298cd41e35a7ffa5689b1a
zsoltpinter <90318352+zsoltpinter@users.noreply.github.com>
2023-11-23 09:09:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.title("Calculator App using Streamlit")
 
 st.write("---")
 
-num1 = st.number_input(lebal="Enter first number")
-num2 = st.number_input(lebal="Enter second number")
+num1 = st.number_input(label="Enter first number")
+num2 = st.number_input(label="Enter second number")
 st.write("Operation")
 
-operation = st.radio("Select an operation to perform:")
+operation = st.radio("Select an operation to perform:",("Összeadás", "Kivonás", "Szorzás", "Osztás"))
 
 
 
