c14694a4140ce0d82ddd3a8fef88b8eabea05c19
zsoltpinter <90318352+zsoltpinter@users.noreply.github.com>
2023-11-23 09:26:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,21 @@ st.write("Operation")
 operation = st.radio("Select an operation to perform:",("Összeadás", "Kivonás", "Szorzás", "Osztás"))
 
 ans = 0
-def calculate():
-  if operation == "Összeadás":
-    ans = num1 + num2
-  elif operation == "Kivonás":
-    ans = num1 - num2
-  elif operation == "Szorzás":
-    ans = num1 * num2
-  elif operation == "Osztás" and num2!=0:
-    ans = num1 / num2
-  else:
-    st.warning("Nem jó, szép volt majd legközelebb.")
-    ans = "XDDD"
 
-st.success(f"EREDMÉNY = {ans}")
+def calculate():
+    if operation == "Add":
+        ans = num1 + num2
+    elif operation == "Subtract":
+        ans = num1 - num2
+    elif operation == "Multiply":
+        ans = num1 * num2
+    elif operation=="Divide" and num2!=0:
+        ans = num1 / num2
+    else:
+        st.warning("Sillo Laurának nem tetszik ez.")
+        ans = "XDDDDD"
+
+    st.success(f"Answer = {ans}")
     
 
 
