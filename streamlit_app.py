a45901aea87ba87950df5cfe3499481a3881f143
zsoltpinter <90318352+zsoltpinter@users.noreply.github.com>
2023-11-23 09:07:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+st.title("Calculator App using Streamlit")
+
+st.write("---")
+
+num1 = st.number_input(lebal="Enter first number")
+num2 = st.number_input(lebal="Enter second number")
+st.write("Operation")
+
+operation = st.radio("Select an operation to perform:")
+
+
 
