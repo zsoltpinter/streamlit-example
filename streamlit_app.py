0d47472b6aee93ed1b7f3d2abb030148c3bda122
zsoltpinter <90318352+zsoltpinter@users.noreply.github.com>
2023-11-23 13:39:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,66 @@ szam2 = st.number_input(label = "OTP Nemzeti Bank")
 st.write("Mit akarsz?")
 st.radio("Lehetőségek :",("Hitel", "KamatosKamat", "Kölcsön", "Pénz felvétel"))
 
+ans = 0 
+def calculate():
+  if Lehetőségek == "Hitel":
+    ans = szam1 + szam2
+  elif Lehetőségek == "KamatosKamat":
+    ans = szam1 - szam2
+  elif Lehetőségek == "Kölcsön":
+    ans = szam1 * szam2
+  elif Lehetőségek == "Pénz felvétel" and number2!=0:
+    ans = szam1 : szam2
+  else:
+    st.warning("Nem vehetsz fel semmit te barom!!44!4!!!!!4!")
+    ans = "NoNo"
+
+st.succes(f"Tessek {ans}")
+
+if st.button("Kalkulalas"):
+  calculate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
