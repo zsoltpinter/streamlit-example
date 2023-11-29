eb38a7286bf8100ee2a1f4380eebefc9506508dd
zsoltpinter <90318352+zsoltpinter@users.noreply.github.com>
2023-11-29 14:19:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,17 @@ szam1 = st.number_input(label = "K&H")
 szam2 = st.number_input(label = "OTP Nemzeti Bank")
 
 st.write("lehetosegek")
-lehetosegek = st.radio("lehetosegek :",("Osszeadas", "Kivonas", "Szorzas", "Osztas"))
+lehetosegek = st.radio("lehetosegek :",("osszeadas", "kivonas", "szorzas", "osztas"))
 
 ans = 0 
 def calculate():
-  if lehetosegek == "Osszeadas":
+  if lehetosegek == "osszeadas":
     ans = szam1 + szam2
-  elif lehetosegek == "Kivonas":
+  elif lehetosegek == "kivonas":
     ans = szam1 - szam2
-  elif lehetosegek == "Szorzas":
+  elif lehetosegek == "szorzas":
     ans = szam1 * szam2
-  elif lehetosegek == "Osztas" and number2!=0:
+  elif lehetosegek == "osztas" and number2!=0:
     ans = szam1 / szam2
   else:
     st.warning("Nem vehetsz fel semmit te barom!!44!4!!!!!4!")
